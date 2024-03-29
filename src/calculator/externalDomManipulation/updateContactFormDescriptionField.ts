3138925fa98181ff5d5be72d3f9e62cd3a7879ca
Roman Nguyen <march08@outlook.com>
2024-03-29 14:47:44 +0100

    next build

--- src/calculator/externalDomManipulation/updateContactFormDescriptionField.ts
+++ src/calculator/externalDomManipulation/updateContactFormDescriptionField.ts
@@ -1,7 +1,7 @@
 export const updateContactFormDescriptionField = (allResText: string, totalImpactText: string) => {
 	if (typeof document !== 'undefined') {
 		const textAreaEl: HTMLTextAreaElement | null = document.querySelector(
-			'#gate-contact-form textarea[name=description]'
+			'.gate-a6428bda-8a1c-4dfc-9866-5232101b2e52 textarea[name=description]'
 		);
 
 		if (textAreaEl) {
