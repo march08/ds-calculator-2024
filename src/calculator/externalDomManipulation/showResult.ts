5bd123261b34e1780009e12ed0c051cea14b4b4b
Roman Nguyen <march08@outlook.com>
2024-03-29 14:33:36 +0100

    fix timing

--- src/calculator/externalDomManipulation/showResult.ts
+++ src/calculator/externalDomManipulation/showResult.ts
@@ -8,16 +8,16 @@ export const toggleResult = (
 		if (resultContainer.getAttribute('data-ds-initialized') === 'true') {
 			if (visible) {
 				onCalculateAnimationStart();
+
 				if (resultContainer) {
 					onToggleResultVisibility();
 					setTimeout(() => {
 						resultContainer.style.display = 'block';
-					}, 1500);
+						setTimeout(() => {
+							document.getElementById('ds-calc-cta-update-container')?.scrollIntoView();
+						}, 500);
+					}, 500);
 				}
-
-				setTimeout(() => {
-					document.getElementById('ds-calc-cta-update-container')?.scrollIntoView();
-				}, 3000);
 			} else {
 				setTimeout(() => {
 					onToggleResultVisibility();
