1ef80693073d33f377f97c44078321c02c91d63d
Roman Nguyen <march08@outlook.com>
2024-05-10 18:26:18 +0200

    locales

--- src/calculator/lang/getConfigByLang.ts
+++ src/calculator/lang/getConfigByLang.ts
@@ -2,7 +2,7 @@ import * as en from './config.js';
 import * as frca from './config.fr-ca.js';
 import * as gb from './config.en-gb.js';
 import * as enca from './config.en-ca.js';
-import * as enau from './config.en-ca.js';
+import * as enau from './config.en-au.js';
 import type { TranslationState } from '../types.js';
 
 const getTranslationData = (lang: string) => {
